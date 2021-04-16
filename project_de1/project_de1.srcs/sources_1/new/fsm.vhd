8f96f10e655164eba6cca51d5f575f5d3993072d
xcadaj00 <xcadaj00@vutbr.cz>
2021-04-16 20:32:27 +0200

    vivado add top module design file, commented things from traffic light

--- project_de1/project_de1.srcs/sources_1/new/fsm.vhd
+++ project_de1/project_de1.srcs/sources_1/new/fsm.vhd
@@ -35,12 +35,12 @@ use ieee.numeric_std.all;
 entity fsm is
     Port ( 
         clk     : in  std_logic;
-        reset   : in  std_logic;
-        south_i : in  std_logic; -- South sensor
-        west_i  : in  std_logic; -- West sensor
-        -- Traffic lights (RGB LEDs) for two directions
-        south_o : out std_logic_vector(3 - 1 downto 0);
-        west_o  : out std_logic_vector(3 - 1 downto 0)
+        reset   : in  std_logic
+--        south_i : in  std_logic; -- South sensor
+--        west_i  : in  std_logic; -- West sensor
+--        -- Traffic lights (RGB LEDs) for two directions
+--        south_o : out std_logic_vector(3 - 1 downto 0);
+--        west_o  : out std_logic_vector(3 - 1 downto 0)
     );
 end fsm;
 
@@ -61,7 +61,7 @@ architecture Behavioral of fsm is
     -- Internal clock enable
     signal s_en     : std_logic;
     -- Local delay counter
-    signal   s_cnt  : unsigned(5 - 1 downto 0);
+    signal s_cnt    : unsigned(5 - 1 downto 0);
 
     -- Specific values for local counter
     constant c_DELAY_2SEC   : unsigned(5 - 1 downto 0) := b"0_1000";
@@ -103,57 +103,57 @@ begin
                 -- Every 250 ms, CASE checks the value of the s_state 
                 -- variable and changes to the next state according 
                 -- to the delay value.
-                case s_state is
+--                case s_state is
 
-                    when goS =>
-                        if (s_cnt < c_DELAY_3SEC) then
-                            s_cnt <= s_cnt + 1;
-                        elsif (west_i = '1') then
-                            -- Move to the next state
-                            s_state <= waitS;
-                            -- Reset local counter value
-                            s_cnt   <= c_ZERO;
-                        end if;
+--                    when goS =>
+--                        if (s_cnt < c_DELAY_3SEC) then
+--                            s_cnt <= s_cnt + 1;
+--                        elsif (west_i = '1') then
+--                            -- Move to the next state
+--                            s_state <= waitS;
+--                            -- Reset local counter value
+--                            s_cnt   <= c_ZERO;
+--                        end if;
 
-                    when waitS =>
-                        -- WRITE YOUR CODE HERE
-                        if (s_cnt < c_DELAY_0p5SEC) then
-                            s_cnt <= s_cnt + 1;
-                        else
-                            -- Move to the next state
-                            s_state <= goW;
-                            -- Reset local counter value
-                            s_cnt   <= c_ZERO;
-                        end if;
+--                    when waitS =>
+--                        -- WRITE YOUR CODE HERE
+--                        if (s_cnt < c_DELAY_0p5SEC) then
+--                            s_cnt <= s_cnt + 1;
+--                        else
+--                            -- Move to the next state
+--                            s_state <= goW;
+--                            -- Reset local counter value
+--                            s_cnt   <= c_ZERO;
+--                        end if;
                     
-                    when goW =>
-                        if (s_cnt < c_DELAY_3SEC) then
-                            s_cnt <= s_cnt + 1;
-                        elsif (south_i = '1') then
-                            -- Move to the next state
-                            s_state <= waitW;
-                            -- Reset local counter value
-                            s_cnt   <= c_ZERO;
-                        end if;
+--                    when goW =>
+--                        if (s_cnt < c_DELAY_3SEC) then
+--                            s_cnt <= s_cnt + 1;
+--                        elsif (south_i = '1') then
+--                            -- Move to the next state
+--                            s_state <= waitW;
+--                            -- Reset local counter value
+--                            s_cnt   <= c_ZERO;
+--                        end if;
                         
-                    when waitW =>
-                        -- WRITE YOUR CODE HERE
-                        if (s_cnt < c_DELAY_0p5SEC) then
-                            s_cnt <= s_cnt + 1;
-                        else
-                            -- Move to the next state
-                            s_state <= goS;
-                            -- Reset local counter value
-                            s_cnt   <= c_ZERO;
-                        end if;
+--                    when waitW =>
+--                        -- WRITE YOUR CODE HERE
+--                        if (s_cnt < c_DELAY_0p5SEC) then
+--                            s_cnt <= s_cnt + 1;
+--                        else
+--                            -- Move to the next state
+--                            s_state <= goS;
+--                            -- Reset local counter value
+--                            s_cnt   <= c_ZERO;
+--                        end if;
                         
-                    -- It is a good programming practice to use the 
-                    -- OTHERS clause, even if all CASE choices have 
-                    -- been made. 
-                    when others =>
-                        s_state <= goS;
+--                    -- It is a good programming practice to use the 
+--                    -- OTHERS clause, even if all CASE choices have 
+--                    -- been made. 
+--                    when others =>
+--                        s_state <= goS;
 
-                end case;
+--                end case;
             end if; -- Synchronous reset
         end if; -- Rising edge
     end process p_doorlock_fsm;
@@ -166,25 +166,25 @@ begin
     --------------------------------------------------------------------
     p_output_fsm : process(s_state)
     begin
-        case s_state is
-                -- WRITE YOUR CODE HERE
-            when goW =>
-                south_o <= "100";  -- Red
-                west_o  <= "010";  -- Green
-            when waitW =>
-                south_o <= "100";  -- Red
-                west_o  <= "110";  -- Yellow
-            when goS =>
-                south_o <= "010";  -- Green
-                west_o  <= "100";  -- Red
-            when waitS =>
-                south_o <= "110";  -- Yellow
-                west_o  <= "100";  -- Red
+--        case s_state is
+--            -- WRITE YOUR CODE HERE
+--            when goW =>
+--                south_o <= "100";  -- Red
+--                west_o  <= "010";  -- Green
+--            when waitW =>
+--                south_o <= "100";  -- Red
+--                west_o  <= "110";  -- Yellow
+--            when goS =>
+--                south_o <= "010";  -- Green
+--                west_o  <= "100";  -- Red
+--            when waitS =>
+--                south_o <= "110";  -- Yellow
+--                west_o  <= "100";  -- Red
 
-            when others =>
-                south_o <= "100";  -- Red
-                west_o  <= "100";  -- Red
-        end case;
+--            when others =>
+--                south_o <= "100";  -- Red
+--                west_o  <= "100";  -- Red
+--        end case;
     end process p_output_fsm;
 
 end Behavioral;
